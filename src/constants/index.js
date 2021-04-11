d2130f915b598aa4549512977865b606688fa620
AndreuSerraSastre <andreuserrasastre@kuarasoftware.com>
2021-04-11 12:48:08 +0200

    cambiado url

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,5 @@
 export const AUTHORIZATION = 'AUTHORIZATION';
-export const URL = 'http://localhost:3000/json/';
+export const URL = process.env.NODE_ENV === 'production'
+    ? 'https://hoteles-free-covid.herokuapp.com/json/' : 'http://localhost:3000/json/';
 export const HOTELESGET = 'hotel.json';
 export const HOTELES = 'hoteles';
